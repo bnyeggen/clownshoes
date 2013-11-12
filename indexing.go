b6387d01c90bd5c430f4aff05eba895ae0156f58
Bryce Nyeggen <fiat.money@gmail.com>
2013-11-11 21:20:29 -0800

    Update the index format

--- indexing.go
+++ indexing.go
@@ -1,8 +1,8 @@
 package clownshoes
 
 import (
-	"bufio"
 	"os"
+	"syscall"
 )
 
 //Indexes have to be in memory for performance anyway, so we store them as
@@ -56,61 +56,99 @@ func (db *DocumentBundle) RemoveIndex(indexName string) {
 	delete(db.indexes, indexName)
 }
 
-// Store the index to a file as a packed series of records.
+// Store the indexes to a file as a packed series of index records.
 // Each record is:
-// An uint32 representing the number of bytes in the key value
-// An uint32 representing the number of offsets
+// A uint64 representing the total number of bytes in the record
+// A uint32 representing the number of bytes in the index name
+// The index name
+// And then for each key / offset pair:
+// An uint32 representing the number of bytes in the key
+// An uint32 representing the number of bytes in the offsets (not the number of elements)
 // The key value
 // All of the offsets for that index name / key, as uint64s
-func (db *DocumentBundle) DumpIndex(indexName string, outfile string) {
-	f, _ := os.OpenFile(outfile, os.O_RDWR|os.O_CREATE, 0666)
-	w := bufio.NewWriter(f)
-	defer w.Flush()
+func (db *DocumentBundle) DumpIndexes(outfile string) {
+	f, _ := os.OpenFile(outfile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
 	defer f.Close()
 
 	buffer := make([]byte, 8)
 
-	for key, positions := range db.indexes[indexName].lookup {
-		keyAsBytes := []byte(key)
-		uint32ToBytes(buffer, 0, uint32(len(keyAsBytes)))
-		uint32ToBytes(buffer, 4, uint32(len(positions)))
-		w.Write(buffer)
-		w.Write(keyAsBytes)
-		for _, position := range positions {
-			uint64ToBytes(buffer, 0, position)
-			w.Write(buffer[:8])
+	for idxName, idx := range db.indexes {
+		indexBuffer := make([]byte, 12, 4096)
+		idxNameAsBytes := []byte(idxName)
+		//Write length of index name
+		uint32ToBytes(indexBuffer, 8, uint32(len(idxNameAsBytes)))
+		//Write index name
+		indexBuffer = append(indexBuffer, idxNameAsBytes...)
+
+		for key, positions := range idx.lookup {
+			keyAsBytes := []byte(key)
+			uint32ToBytes(buffer, 0, uint32(len(keyAsBytes)))
+			uint32ToBytes(buffer, 4, uint32(len(positions)*8))
+			//Write length of key, and length of positions buffer
+			indexBuffer = append(indexBuffer, buffer...)
+			//Write key
+			indexBuffer = append(indexBuffer, keyAsBytes...)
+
+			//Group the positions data
+			buffer = make([]byte, len(positions)*8)
+			for i, position := range positions {
+				uint64ToBytes(buffer, uint64(i*8), position)
+			}
+
+			//Write positions buffer
+			indexBuffer = append(indexBuffer, buffer...)
 		}
+		//Now that we know the total length of the index buffer, write it at the beginning
+		uint64ToBytes(indexBuffer, 0, uint64(len(indexBuffer)))
+		//And put it on disk
+		f.Write(indexBuffer)
 	}
 }
 
-// Load the index to the given DB with the given name, and use the given keyFn
-// to update it in the future.  Assumes the correctness of the index.
-func (db *DocumentBundle) LoadIndex(keyFn func([]byte) string, indexName string, outfile string) (out index) {
-	f, _ := os.Open(outfile)
-	r := bufio.NewReader(f)
-	defer f.Close()
+// Load the packed indexes from the given file, using the supplied map to associate
+// the appropriate key function with them going forward.  Set the db's indexes as
+// such.  Assumes the index is valid & up-to-date.
+func (db *DocumentBundle) LoadIndexes(nameToKeyFns map[string]func([]byte) string, indexFile string) {
+	f, _ := os.Open(indexFile)
+	stats, _ := f.Stat()
+	//I love mmaping
+	buffer, _ := syscall.Mmap(int(f.Fd()), 0, int(stats.Size()), syscall.PROT_READ, syscall.MAP_SHARED)
+	pos := uint64(0)
+	out := make(map[string]index)
 
-	out.keyFn = keyFn
+	for pos < uint64(len(buffer)) {
+		var thisIdx index
 
-	buffer := make([]byte, 8)
-	for {
-		_, e := r.Read(buffer)
-		if e != nil {
-			return out
+		bytesRead := uint64(0)
+		idxLength := uint64FromBytes(buffer, pos)
+		bytesRead += 8
+		idxNameLength := uint64(uint32FromBytes(buffer, pos+bytesRead))
+		bytesRead += 4
+		idxName := string(buffer[pos+bytesRead : pos+bytesRead+idxNameLength])
+		bytesRead += idxNameLength
+
+		thisIdx.keyFn = nameToKeyFns[idxName]
+		thisIdx.lookup = make(map[string][]uint64)
+
+		for uint64(bytesRead) < idxLength {
+			keyLength := uint64(uint32FromBytes(buffer, pos+bytesRead))
+			bytesRead += 4
+			offsetLength := uint64(uint32FromBytes(buffer, pos+bytesRead))
+			bytesRead += 4
+			key := string(buffer[pos+bytesRead : pos+bytesRead+keyLength])
+			bytesRead += keyLength
+			nOffsets := offsetLength / 8
+			offsets := make([]uint64, nOffsets)
+			for i := uint64(0); i < nOffsets; i++ {
+				offsets[i] = uint64FromBytes(buffer, pos+bytesRead)
+				bytesRead += 8
+			}
+			thisIdx.lookup[key] = offsets
 		}
-		keyLength := uint32FromBytes(buffer, 0)
-		nOffsets := uint32FromBytes(buffer, 4)
 
-		keyBuffer := make([]byte, keyLength)
-		r.Read(keyBuffer)
-
-		offsetBuffer := make([]byte, nOffsets*8)
-		r.Read(offsetBuffer)
-
-		offsets := make([]uint64, nOffsets)
-		for i := uint32(0); i < nOffsets; i++ {
-			offsets[i] = uint64FromBytes(offsetBuffer, 0)
-			offsetBuffer = offsetBuffer[8:]
-		}
+		out[idxName] = thisIdx
+		pos += idxLength
 	}
+
+	db.indexes = out
 }
