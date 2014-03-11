9f2b518db25b0de64980941dce6323e6c26cf622
Bryce Nyeggen <fiat.money@gmail.com>
2014-03-10 20:45:03 -0700

    Test indexed update

--- indexing_test.go
+++ indexing_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -91,5 +92,42 @@ func TestIndexing(t *testing.T) {
 			t.Log(len(v))
 			t.Error("Error in indexed retrieval after indexed deletion")
 		}
+	}
+}
+
+func TestDBUpdateWhere(t *testing.T) {
+	f, e := ioutil.TempFile("", "ClownshoesDBTest")
+	if e != nil {
+		t.Error("Problem creating db", e)
+	}
+	f.Close()
+	db := NewDB(f.Name())
+	defer os.Remove(f.Name())
+
+	doc1 := NewDocument([]byte("alpha"))
+	doc2 := NewDocument([]byte("beta"))
+	doc3 := NewDocument([]byte("gamma"))
+	db.PutDocument(doc1)
+	db.PutDocument(doc2)
+	db.PutDocument(doc3)
+
+	db.AddIndex("identity", func(b []byte) string { return string(b) })
+
+	upcaser := func(input []byte) ([]byte, bool) {
+		strInput := string(input)
+		up := strings.ToUpper(strInput)
+		if up != strInput {
+			return []byte(up), true
+		}
+		return input, false
+	}
+
+	ct := db.ReplaceDocumentsWhere("identity", "alpha", upcaser)
+	if ct != 1 {
+		t.Error("Insufficient documents replaced")
+	}
+	docs := db.GetDocumentsWhere("identity", "ALPHA")
+	if len(docs) != 1 {
+		t.Error("Indexed documents not retrieved after indexed update")
 	}
 }
